c00b7c96a2d54d3251bfa13e3647a728578852eb
ptr25 <tr.pranathi@gmail.com>
2020-12-01 13:09:31 +0530

    storyhub

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,Image} from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs'
+import WriteStoryScreen from './screens/WriteStoryScreen.js'
+import ReadStoryScreen from './screens/ReadStoryScreen.js'
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AppContainer/>
   );
 }
+const TabNavigator = createBottomTabNavigator({
+  WriteStoryScreen:{screen:WriteStoryScreen},
+  ReadStoryScreen :{screen:ReadStoryScreen}
+},
+{
+  defaultNavigationOptions:({navigation})=>({
+    tabBarIcon:({})=>{
+      const routeName = navigation.state.routeName
+      if(routeName==='WriteStoryScreen'){
+        return(
+          <Image source={require('./assets/write.png')}
+          style = {{width:40,
+                    height:40}}/>
+        )}
+      else if(routeName==='ReadStoryScreen'){
+        return(
+          <Image source={require('./assets/read.png')}
+          style = {{width:40,
+                    height:40}}/>
+        )
+      }
+    }
+  })
+}
+)
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const AppContainer = createAppContainer(TabNavigator)
